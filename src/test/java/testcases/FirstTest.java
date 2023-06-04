ea8808ec1cb7b051f9934c6173974b5018d872b8
annatimofeyeva <anjatimofeyeva@gmail.com>
2023-06-04 12:14:27 -0700

    project structure configured; FirstTest class added

---
+++ src/test/java/testcases/FirstTest.java
@@ -0,0 +1,18 @@
+package testcases;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    @Test
+    public void startBrowser() {
+        System.setProperty("webdriver.chrome.driver", "/home/anya/IdeaProjects/SeleniumProject/src/test/resources/chromedriver");
+        // I connected Java 11 to the Project, so I have to add ChromeOptions below
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("https://www.nytimes.com/");
+        driver.manage().window().maximize();
+        driver.close();
+    }
+}
